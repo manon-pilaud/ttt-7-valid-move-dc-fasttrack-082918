0f20cde193ef0f08435045b48bbb73204f2b9bff
Mandy Pilaud <mandyypilaud@gmail.com>
2018-09-30 17:17:47 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,8 @@
 def valid_move?(board,index)
   if position_taken?(board,index)
     return false 
-  elsif index > 8 || index< 0
+  #elsif index > 8 || index< 0
+  elslif index.between?(0,8)
     return nil
   else 
     return true 
